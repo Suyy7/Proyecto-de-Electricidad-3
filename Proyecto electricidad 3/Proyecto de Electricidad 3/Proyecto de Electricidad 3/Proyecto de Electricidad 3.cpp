aa2654bf4f45f278cd16553086183fdc8f4e7220
Suyy7 <saagea7@gmail.com>
2024-06-10 20:43:44 -0300

    10/06 Agregamos las formulas matematicas al main

--- Proyecto electricidad 3/Proyecto de Electricidad 3/Proyecto de Electricidad 3/Proyecto de Electricidad 3.cpp
+++ Proyecto electricidad 3/Proyecto de Electricidad 3/Proyecto de Electricidad 3/Proyecto de Electricidad 3.cpp
@@ -3,9 +3,16 @@
 #include <string.h>
 using namespace std;
 // List cross
+string usuariobase = "suy";
+string contraBase = "12345";
+
 
 struct lista {
-    int date;
+    string usuario;
+    string contraseña;
+    string nombre;
+    string profesion;
+    
     lista* sig;
 };
 
@@ -84,8 +91,20 @@ IngenieroElectrico::~IngenieroElectrico()
 static void menu();
 void ingresoNuevo();
 bool comparacion(string, string);
+bool comparacion2(string, string, string, string);
 void ingresoSistema();
 bool preguntas(string rpta);
+// Prototipe function of electrical 
+
+float qulomb(float, float, float, float);
+float intensidadA(float, float);
+float ohmR(float, float);
+float tensionV(float, float);
+float potencia(float, float);
+float potenciaAp(float, float);
+float calorQ(float, float, float);
+float energia(float, float);
+
 
 
 int main()
@@ -115,20 +134,23 @@ static void menu() {
 
     if (rpta == "si") {
         ingresoNuevo(); // Lista 
+        ingresoSistema();
     }
     else
     {  // Si no es nuevo ingresa al sistema. 
-        cout << "\n Ingrese su contraseña y usuario. ";
-        cout << "\n Usuario: ";  cin >> usuario;
-        cout << "\n Contraseña: "; cin >> contraseña;
-        if (comparacion(usuario, contraseña) && comparacion(usuario,contraseña)) {
-            // Ingreso al sistema;
-        }
-        else {  // Mensaje de fallo
-            cout << "\n Error. ";
-            cout << "\n Verifique nuevamente el usuario o la contraseña. ";
-            cout << "\n vuelva a intentarlo. ";
-        }
+        do {
+            cout << "\n Ingrese su contraseña y usuario. ";
+            cout << "\n Usuario: ";  cin >> usuario;
+            cout << "\n Contraseña: "; cin >> contraseña;
+            if (comparacion2(usuariobase,usuario,contraBase,contraseña)) {
+                ingresoSistema(); // Ingreso al sistema. 
+            }
+            else {  // Mensaje de fallo
+                cout << "\n Error. ";
+                cout << "\n Verifique nuevamente el usuario o la contraseña. ";
+                cout << "\n vuelva a intentarlo. ";
+            }
+        } while (comparacion2(usuariobase, usuario, contraBase, contraseña) != true);
     }
 
 
@@ -150,6 +172,7 @@ void ingresoNuevo() {
     cout << "\n Dime tu usuario: "; cin >> nUsuario;
     cout << "\n Dime tu contraseña: "; cin >> contraseña;
     cout << "\n Dime tu actividad: "; cin >> actividad;
+    
 
 };
 
@@ -157,7 +180,7 @@ void ingresoNuevo() {
 bool comparacion(string a, string b) { // comparacion de char sea contra o usuario. 
     return (a == b) ? true : false;
 };
-bool comparacion(string a, string b, string c, string d) { // comparacion de char sea contra o usuario. 
+bool comparacion2(string a, string b, string c, string d) { // Comparacion para los ingresos de las personas.
     return (a == b && c == d) ? true : false;
 };
 void ingresoSistema() {
@@ -171,11 +194,13 @@ void ingresoSistema() {
         cout << "\n - 4. Herramientas de medicion y sus usos. " << endl;
         cout << "\n - 5. Contactos de recomendacion. " << endl;
         cout << "\n - 6. Ayuda de la empresa. " << endl;
+        cout << "\n - 7. Desea salir? . " << endl;
         cout << "\n Aqui >>>"; cin >> opc;
 
     switch (opc)
     {
     case 1:
+        throw;
         
         break;
     case 2:
@@ -205,4 +230,32 @@ bool preguntas(string rpta) { // funcion depregunta para la Asignacion de respue
     else {
         return false;
     }
+};
+
+float qulomb(float a, float b, float c, float d) {
+    return a = b * ((c * c) / pow(d, 2));
+};
+
+float intensidadA(float a, float b) {
+    return a / b;
+};
+float ohmR(float a, float b) {
+    return a / b;
+};
+float tensionV(float a, float b) {
+    return a * b;
+};
+float potencia(float a, float b) {
+    return a * b;
+};
+float potenciaAp(float a, float b) {
+    return sqrt((a * a) + (b * b));
+
+};
+float calorQ(float a, float b, float t) {
+    return (a * a) * b * t;
+};
+float energia(float a, float b) {
+    return a * b;
+
 };
